4efc6548357555e948cab2bb2096b50869ffc3f6
jonathan gerber <jlgerber@gmail.com>
2018-04-15 15:35:53 -0700

    update traits

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,14 @@
 use std::{ marker, slice::Iter };
 
-/// Provide a way to iterate over enum values and convert them from
-/// str
-pub trait EnumEase {
+/// Iterate over an Enum's values in the order in which they
+/// are defined (top down)
+pub trait EnumIter {
     fn iterator() -> Iter<'static, Self> where Self: marker::Sized;
-    fn from_str(key: &str) -> Option<Self> where Self: marker::Sized;
 }
 
+/// Generate an enum from a &str. If the &str matches an enum value,
+/// return Some(value), otherwise, return None. (At some point,
+/// this trait may go away in favor of the nightly-only TryFrom trait )
+pub trait EnumFromStr: EnumIter {
+    fn from_str(key: &str) -> Option<Self> where Self: marker::Sized;
+}
